981023f2f3e37c844686f771ebfd535e2b6cf6dc
apourbo <69768031+apourbo@users.noreply.github.com>
2023-11-19 17:19:05 +0600

    Create pikuthechatbot.sh

---
+++ pikuthechatbot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+sudo curl -sSL https://raw.githubusercontent.com/aandrew-me/tgpt/main/install | bash -s /usr/local/bin
+
+sudo mv /usr/local/bin/tpgt /usr/local/bin/piku
+
+echo "Janu Setup completed, just type piku '<whateveryouwant>', check the readme file for more..... Love you imaaaa ummmmmmmmmah"
+
